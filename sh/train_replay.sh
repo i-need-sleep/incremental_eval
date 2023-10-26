c4caca3f7615d46491b2e9fbea0394f31980b775
yichen huang <yh2689@nyu.edu>
2023-10-27 00:34:15 +0400

    sh

--- sh/train_replay.sh
+++ sh/train_replay.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-#SBATCH --job-name=debug       # 任务名
+#SBATCH --job-name=train_replay       # 任务名
 #SBATCH --nodes=1                   # 这里不用动 多节点脚本请查官方文档
 #SBATCH --ntasks=1                  # 这里不用动 多任务脚本请查官方文档
 #SBATCH --cpus-per-task=4           # 要几块CPU (一般4块就够用了)
-#SBATCH --mem=64GB                 # 最大内存
+#SBATCH --mem=256GB                 # 最大内存
 #SBATCH --time=12:00:00           # 运行时间上限
 #SBATCH --mail-type=END             # ALL / END
 #SBATCH --mail-user=yh2689@nyu.edu  # 结束之后给哪里发邮件
 #SBATCH --output=./logs/%x%A.out           # 正常输出写入的文件
 #SBATCH --error=./logs/%x%A.err            # 报错信息写入的文件
-#SBATCH -q cpu-512                  # 有GPU的partition
+#SBATCH --gres=gpu:1                # 需要几块GPU (同时最多8块)
+#SBATCH -p gpu                   # 有GPU的partition
+#SBATCH --qos=gpu-8                 # To enable the use of up to 8 GPUs
 
 nvidia-smi
 nvcc --version
@@ -21,6 +23,5 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/
 export HF_HOME=/l/users/yichen.huang/misc/cache
 python -u training.py \
     --name train_replay \
-    --strategy replay \
-    --debug
+    --strategy replay
 echo "FINISH"                       # 输出起始信息
